73061311dcd0ce72de9204a32bb1a416ff0e63f7
Henry Müssemann <h.muessemann@gmx.de>
2017-03-02 12:50:31 +0100

    Made timestamps datetime and added 'if not exists'. Ref T5

--- PeachBackend/sql/workflow_execution_queue.sql
+++ PeachBackend/sql/workflow_execution_queue.sql
@@ -1,4 +1,4 @@
-CREATE TABLE queue (
+CREATE TABLE IF NOT EXISTS queue (
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
     user VARCHAR(30),
     execution_file VARCHAR(255) UNIQUE,
@@ -6,8 +6,8 @@ CREATE TABLE queue (
     status INT(1),
     progress INT(3),
     original_workflow_file VARCHAR(255),
-    sending_date TIMESTAMP,
-    finished_date TIMESTAMP,
+    sending_date datetime NOT NULL DEFAULT '1000-01-01',
+    finished_date datetime NOT NULL DEFAULT '1000-01-01',
     priority INT(1),
     output_file VARCHAR(255)
 );
